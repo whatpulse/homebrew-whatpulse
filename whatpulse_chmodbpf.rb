0dcccc04dd93afa0730051a7c63b32652bf305b3
Martijn Smit <martijn@lostdomain.org>
2023-09-22 20:10:42 +0200

    Add version

--- whatpulse_chmodbpf.rb
+++ whatpulse_chmodbpf.rb
@@ -1,6 +1,7 @@
 class WhatpulseChmodbpf < Formula
   desc 'WhatPulse ChmodBPF Installer (required for networking stats)'
   homepage 'https://whatpulse.org'
+  version '1.0'
 
   url 'https://releases.whatpulse.org/latest/macos-arm/whatpulse-mac-arm-latest.dmg'
   sha256 'de24ae635bacdabbd785a6427b37b31bf8802b4ee015f6e417f325adad420139'
