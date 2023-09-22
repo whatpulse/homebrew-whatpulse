8404d1003b1a4c9a2a7a87ec66bcff113839575f
Martijn Smit <martijn@lostdomain.org>
2023-09-22 19:05:38 +0200

    Bump to v5.5.2 and try something for headless installs

--- whatpulse.rb
+++ whatpulse.rb
@@ -1,14 +1,14 @@
 class Whatpulse < Formula
   desc 'WhatPulse'
   homepage 'https://whatpulse.org'
-  version '5.3'
+  version '5.5.2'
 
   if Hardware::CPU.arm?
     url 'https://releases.whatpulse.org/latest/macos-arm/whatpulse-mac-arm-latest.dmg'
-    sha256 '4bf4871277ded9a8ac56503f2997f4c83148d6d1be6623560c631c1237874701'
+    sha256 '15e447cfbfa31ea70eefd63433ce5f57f60577069eb0c68c1792a501cbfc3a42'
   else
     url 'https://releases.whatpulse.org/latest/macos/whatpulse-mac-latest.dmg'
-    sha256 '8e3481d18402a06fc58177d74ce7fffc4531823727d6f80198f38ef3026c766b'
+    sha256 '857d68a303ca005795cd0a43fbb1d13bed2c9672b740ddc00f2ae9d9a278d86a'
   end
 
   def install
@@ -21,6 +21,10 @@ class Whatpulse < Formula
   # open the installer. this cannot be an unattended install, as brew launches commands in a
   # headless console, and the installer (even the CLI installer) requires a head. :-(
   def post_install
-    system 'open', "#{prefix}/WhatPulse-#{version}-Installer.app"
+    installer_path = "#{prefix}/WhatPulse-#{version}-Installer.app/Contents/MacOS/WhatPulse-#{version}-Installer"
+    params = '--root /Applications/WhatPulse552 --accept-messages --accept-licenses --confirm-command install'
+    install_command = "#{installer_path} #{params}"
+
+    system(install_command)
   end
 end
