5fb3539bc487bd8e80450c505d74e863363cb1e6
Martijn Smit <martijn@lostdomain.org>
2023-09-23 19:01:27 +0200

    different syntax

--- whatpulse_chmodbpf.rb
+++ whatpulse_chmodbpf.rb
@@ -6,8 +6,5 @@ class WhatpulseChmodbpf < Formula
   url 'https://releases.whatpulse.org/latest/macos/install.ChmodBPF.pkg'
   sha256 '739fe63afe689b19de5df1b391ff702fc39f350348c0d05661432bb742e49483'
 
-  def install
-    # Install the ChmodBPF package as root
-    system 'sudo', 'installer', '-pkg', 'install.ChmodBPF.pkg', '-target', '/'
-  end
+  pkg 'install.ChmodBPF.pkg'
 end
