cb14970a4467414d8c8832b89fbf5e3cb63315b9
Martijn Smit <martijn@lostdomain.org>
2023-09-23 19:15:18 +0200

    formula -> cask ?

--- whatpulse.rb
+++ whatpulse.rb
@@ -1,31 +1,23 @@
-class Whatpulse < Formula
+cask 'whatpulse' do
+  arch arm: 'aarch64', intel: 'x86-64'
+
   desc 'WhatPulse'
   homepage 'https://whatpulse.org'
   version '5.5.2'
-  depends_on 'whatpulse_chmodbpf'
+  depends_on cask: 'whatpulse_chmodbpf'
 
-  if Hardware::CPU.arm?
+  on_arm do
     url 'https://releases.whatpulse.org/latest/macos-arm/whatpulse-mac-arm-latest.dmg'
     sha256 '15e447cfbfa31ea70eefd63433ce5f57f60577069eb0c68c1792a501cbfc3a42'
-  else
+  end
+  on_intel do
     url 'https://releases.whatpulse.org/latest/macos/whatpulse-mac-latest.dmg'
     sha256 '857d68a303ca005795cd0a43fbb1d13bed2c9672b740ddc00f2ae9d9a278d86a'
   end
 
-  def install
-    volume = "/Volumes/WhatPulse\ #{version}"
-    system 'hdiutil', 'attach', '-quiet', '-nobrowse', cached_download
-    cp_r "#{volume}/WhatPulse-#{version}-Installer.app", "#{prefix}/"
-    system 'hdiutil', 'detach', "/Volumes/WhatPulse\ #{version}"
-  end
-
-  # open the installer. this cannot be an unattended install, as brew launches commands in a
-  # headless console, and the installer (even the CLI installer) requires a head. :-(
-  def post_install
-    installer_path = "#{prefix}/WhatPulse-#{version}-Installer.app/Contents/MacOS/WhatPulse-#{version}-Installer"
-    params = "--root /Applications/WhatPulse552 --accept-messages --accept-licenses --confirm-command --cache-path #{prefix}/cache"
-    install_command = "#{installer_path} #{params} install"
-
-    system(install_command)
-  end
+  installer script: {
+    executable: "WhatPulse-#{version}-Installer.app/Contents/MacOS/WhatPulse-#{version}-Installer",
+    args: ['--root', '/Applications/WhatPulse552', '--accept-messages', '--accept-licenses', '--confirm-command',
+           '--cache-path', "#{staged_path}/cache", 'install']
+  }
 end
